ee80b804f12e8d90fcf627d8e253eed910a65461
faizan <faizanr206@gmail.com>
2022-10-08 01:29:20 +0500

    first-Change in front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { IntroComponent } from './intro/intro.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavBarComponent,
+    IntroComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TooltipModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/intro/intro.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+// @ts-ignore
+import Typewriter from 't-writer.js';
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.component.html',
+  styleUrls: ['./intro.component.css']
+})
+export class IntroComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+      const target = document.querySelector('.name')
+      const text_items = document.querySelector('.tw')
+      const writer = new Typewriter(text_items, {
+        loop: true,
+        typeSpeed: 80,
+        deleteSpeed: 80,
+        typeColor: 'white'
+      })
+      
+      writer
+        .type('I\'m Web-Developer')
+        .rest(500)
+        .changeOps({ deleteSpeed: 80 })
+        .remove(13)
+        .type('Designer')
+        .rest(500)
+        .changeOps({ deleteSpeed: 80 })
+        .remove(8)
+        .type('Photographer')
+        .rest(500)
+        .changeOps({ deleteSpeed: 80 })
+        .remove(12)
+        .type('Ceo')
+        .rest(500)
+        .changeOps({ deleteSpeed: 80 })
+        .clear()
+        .start()
+
+        const onetime = new Typewriter(target, {
+          loop: false,
+          typeColor: 'white'
+        })
+        
+        onetime
+          .type('Hi There')
+          .rest(500)
+          .changeOps({ deleteSpeed: 80 })
+          .remove(8)
+          .type('I\'m Faizan')
+          .rest(5)
+          .start()
+    }
+
+}
